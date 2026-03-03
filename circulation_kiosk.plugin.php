61835aef062416f23bdeb330ae4b75f4ea65ecc5
indra-f-r <72896762+indra-f-r@users.noreply.github.com>
2026-03-04 06:05:11 +0700

    Update circulation_kiosk.plugin.php

--- circulation_kiosk.plugin.php
+++ circulation_kiosk.plugin.php
@@ -24,7 +24,7 @@ Plugins::hook(Plugins::CONTENT_BEFORE_LOAD, function () {
     $page = $_GET['p'] ?? '';
     $key  = $_GET['key'] ?? '';
 
-    $TOKEN = 'z0Z6olm5RMred0XoksEliwk4CSTL5TZwomNd4d4X4veOB3zFj3u1jMLlEjgXLLvKFHqQwmUDir4iVGfNsLvtetmG6sb9xMGup4FXgqguE4u17TAhjlRODnevsI8junWUSQH6N9DjSWkhkHsVqw2kMERa1yPfQoeyZYI2QCXPP3p7PzykH2iWDmcojSuc2eLxqD7T4xHyyoBKz8G3kA5T7UmzEANJNl9IsDEXfBR38OM32Nq093iTlX3KDFnVCs4stffRFNaAdEnxMXVLWJzQ8OnT4HzzMOcrQBCcz2c5CrXvHEiiIH7KrUQ1ZDxWH1NtHE3RciZq9uNWhQjqO41lPFmXTKkLdK0t2C1Tpr0YCcGwTYweeVoLEY3R81lLnN0B31EGKfI48MDGsgH8BJwVqnuyEsSPyKd55EHegC68YdF2Zi7xQdAHJjdtTvNFnAiLSau6HsmG2f9J3uweynLptbBWHpnZqE2D7i4M0B6h9yGTFuMmVa4xOESe8pNUH9tJ';
+    $TOKEN = 'YOUR TOKEN';
 
     /*
     |--------------------------------------------------------------------------
@@ -73,6 +73,7 @@ Plugins::hook(Plugins::CONTENT_BEFORE_LOAD, function () {
         require __DIR__ . '/circulation_kiosk.php';
         exit;
     }
+
 
 
 });
